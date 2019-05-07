31a1986ef9ead6d1a838a03b56e968a737078f58
Paul Martin <communty.hivemind@gmail.com>
2019-05-07 11:41:21 +0100

    Refactor #depost tests to use explicit subject

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -2,15 +2,15 @@ require 'account'
 
 describe Account do
   describe '#deposit' do
+    subject(:account) { described_class.new }
+
     it { is_expected.to respond_to(:deposit).with(1).argument }
 
     it "Returns the balance after one deposit made" do
-      account = Account.new
       expect(account.deposit(1000)).to eq 1000
     end
 
     it "Returns the balance after two deposits made" do
-      account = Account.new
       account.deposit(2000)
       expect(account.deposit(1000)).to eq 3000
     end
