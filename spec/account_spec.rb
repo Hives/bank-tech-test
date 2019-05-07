071736c856a3c35e79a62418449eb18e690051cb
Paul Martin <communty.hivemind@gmail.com>
2019-05-07 11:49:22 +0100

    Add another test for deposits + withdrawals
    Make some deposits and withdrawals and check the balance returned is
    correct

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -6,11 +6,11 @@ describe Account do
   describe '#deposit' do
     it { is_expected.to respond_to(:deposit).with(1).argument }
 
-    it "Returns the balance after one deposit made" do
+    it "Returns the correct balance after one deposit made" do
       expect(account.deposit(1000)).to eq 1000
     end
 
-    it "Returns the balance after two deposits made" do
+    it "Returns the correct balance after two deposits made" do
       account.deposit(2000)
       expect(account.deposit(1000)).to eq 3000
     end
@@ -19,13 +19,22 @@ describe Account do
   describe '#withdraw' do
     it { is_expected.to respond_to(:withdraw).with(1).argument }
 
-    it "Returns the balance after one withdrawal made" do
+    it "Returns the correct balance after one withdrawal made" do
       expect(account.withdraw(1000)).to eq -1000
     end
 
-    it "Returns the balance after two withdrawals made" do
+    it "Returns the correct balance after two withdrawals made" do
       account.withdraw(2000)
       expect(account.withdraw(1000)).to eq -3000
+    end
+  end
+
+  describe 'updating account' do
+    it "Returns the correct balance after deposits and withdrawals made" do
+      account.deposit(100)
+      account.deposit(200)
+      account.withdraw(50)
+      expect(account.withdraw(100)).to eq 150
     end
   end
 end
